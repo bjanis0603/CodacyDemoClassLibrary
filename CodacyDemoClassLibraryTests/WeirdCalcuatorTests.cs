8e6d46ef638d41636d530860f41771fc81add5d4
bjanis0603 <bjanis@practicevelocity.com>
2019-03-18 18:20:54 -0500

    TEST

--- CodacyDemoClassLibraryTests/WeirdCalcuatorTests.cs
+++ CodacyDemoClassLibraryTests/WeirdCalcuatorTests.cs
@@ -14,9 +14,7 @@ namespace CodacyDemoClassLibrary.Tests
         [TestMethod()]
         public void DoSomeUnnecessaryCalculationTest()
         {
-            WeirdCalcuator calcuator = new WeirdCalcuator();
-
-            Assert.AreEqual(21, calcuator.DoSomeUnnecessaryCalculation(1));
+            Assert.Fail();
         }
     }
 }
